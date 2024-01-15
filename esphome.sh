743a65d0ba6ceaf22abaf51164cc0594fe83e4a6
Alex <thealexsb@gmail.com>
2024-01-15 10:27:49 +0300

    docker: disable privileged

--- esphome.sh
+++ esphome.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -x
 # -e ESPHOME_DASHBOARD_USE_PING=true
-docker run --name esphome --rm --privileged --net=host -e TZ=Europe/Moscow -v /etc/localtime:/etc/localtime:ro -v "${PWD}":/config -v "${PWD}/.platformio":/root/.platformio -it ghcr.io/esphome/esphome $*
+docker run --name esphome --rm --net=host -e TZ=Europe/Moscow -v /etc/localtime:/etc/localtime:ro -v "${PWD}":/config -v "${PWD}/.platformio":/root/.platformio -it ghcr.io/esphome/esphome $*
