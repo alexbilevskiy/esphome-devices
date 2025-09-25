32f06cd69c66e34779dd25eabc395c2b89dc2120
Alex <thealexsb@gmail.com>
2025-09-25 16:35:11 +0300

    up docker run command

--- esphome.sh
+++ esphome.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
 set -x
+VERS=2025.9.1
 # -e ESPHOME_DASHBOARD_USE_PING=true
-docker run --name esphome --rm --net=host -e TZ=Europe/Moscow -v /etc/localtime:/etc/localtime:ro -v "${PWD}":/config -v "${PWD}/.platformio":/root/.platformio -v "${PWD}/.cache":/cache -v "${PWD}/.build":/build -it ghcr.io/esphome/esphome $*
+docker run \
+  --name esphome \
+  --rm \
+  --net=host \
+  -e TZ=Europe/Moscow \
+  -v /etc/localtime:/etc/localtime:ro \
+  -v "${PWD}":/config \
+  -v "${PWD}/.platformio":/root/.platformio \
+  -v "${PWD}/.cache":/cache \
+  -v "${PWD}/.build":/build \
+  -it ghcr.io/esphome/esphome:$VERS \
+  $*
